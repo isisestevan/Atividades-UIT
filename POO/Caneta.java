5f63d522b378e68da6d701284bb6558bb1360a42
isisestevan <isisestevan@gmail.com>
2021-08-14 17:05:37 -0300

    13/08

---
+++ POO/Caneta.java
@@ -0,0 +1,18 @@
+public class Caneta {
+    String cor;
+    int preco;
+    int tinta;
+    String funcionca;
+
+
+    public void colorir(String cor){
+      cor=this.cor;
+    }
+
+    public void etiquetar(int preco){
+         preco=this.preco;
+    }
+
+  
+}
+//fazer um classe main new caneta e colocar as funçãozinha
